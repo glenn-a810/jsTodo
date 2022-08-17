cfd5538433678464529ba751b4bf74530dd5b801
Glenn <a810.glenn@gmail.com>
2022-08-17 23:34:24 +0900

    component 분리

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, { useState } from 'react'
 import './App.css'
 import List from './components/List'
+import Input from './components/Input'
 
 export default function App() {
   const [todoData, setTodoData] = useState([])
   const [value, setValue] = useState('')
-
-  const handleChange = (e) => {
-    setValue(e.target.value)
-  }
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-
-    let newTodo = {
-      id: Date.now(),
-      title: value,
-      completed: false,
-    }
-
-    setTodoData((prev) => [...prev, newTodo])
-    setValue('')
-  }
 
   return (
     <div className="container">
@@ -29,25 +13,8 @@ export default function App() {
         <div className="title">
           <h1>할 일 목록</h1>
         </div>
-
         <List todoData={todoData} setTodoData={setTodoData} />
-
-        <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
-          <input
-            type="text"
-            name="value"
-            style={{ flex: '10', padding: '5px' }}
-            placeholder="해야 할 일을 입력하세요."
-            value={value}
-            onChange={handleChange}
-          />
-          <input
-            type="submit"
-            value="입력"
-            className="btn"
-            style={{ flex: '1' }}
-          />
-        </form>
+        <Input value={value} setValue={setValue} setTodoData={setTodoData} />
       </div>
     </div>
   )
---
+++ src/components/Input.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Input = ({ value, setValue, setTodoData }) => {
+  const handleChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    let newTodo = {
+      id: Date.now(),
+      title: value,
+      completed: false,
+    }
+
+    setTodoData((prev) => [...prev, newTodo])
+    setValue('')
+  }
+
+  return (
+    <div>
+      <form style={{ display: 'flex' }} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="value"
+          style={{ flex: '10', padding: '5px' }}
+          placeholder="해야 할 일을 입력하세요."
+          value={value}
+          onChange={handleChange}
+        />
+        <input
+          type="submit"
+          value="입력"
+          className="btn"
+          style={{ flex: '1' }}
+        />
+      </form>
+    </div>
+  )
+}
+
+export default Input
