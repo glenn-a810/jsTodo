e568e39af81e44f5da0083760cd07853490748fb
Glenn <a810.glenn@gmail.com>
2022-08-24 20:28:23 +0900

    deleteAll 추가

--- src/App.js
+++ src/App.js
@@ -15,11 +15,16 @@ export default function App() {
     [todoData]
   )
 
+  const handleRemoveAll = () => {
+    setTodoData([])
+  }
+
   return (
     <div className="flex items-center justify-center w-screen h-screen bg-blue-50">
       <div className="w-full p-6 m-4 bg-white rounded shadow lg:w-3/4">
         <div className="flex justify-between mb-3">
           <h1>할 일 목록</h1>
+          <button onClick={handleRemoveAll}>모두 삭제</button>
         </div>
         <Lists
           handleClick={handleClick}
